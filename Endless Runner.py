2045613b8eb648fd0db5f047d2a9a7520278e490
Olipus <dotdecaydk@gmail.com>
2018-11-14 12:50:33 +0100

    Added Game File

---
+++ Endless Runner.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      OLIVER
+#
+# Created:     14-11-2018
+# Copyright:   (c) OLIVER 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import pygame
+pygame.init()
+
+class Cannin:
+    moveSpeed = 10
+
+
+pygame.display.set_mode((500,500))
+print(pygame.display.get_init())
+input("")
+
