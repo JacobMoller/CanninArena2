7f82cd72a843b7eed5de8fd44a3ad1e7e0260dca
Jacob Møller <jmj1999@gmail.com>
2018-11-14 12:49:17 +0100

    Added art & menu code

--- menu.py
+++ menu.py
@@ -4,3 +4,12 @@
 #    Oliver Thejl Eriksen       #
 #   Rasmus Damgaard-Iversen     #
 #################################
+import pygame
+pygame.init()
+
+gameDisplay = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption('CanninArena 2')
+
+clock = pygame.time.Clock()
+
